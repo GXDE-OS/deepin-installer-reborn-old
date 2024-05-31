4297a0f6ab294b013a4cd083e130be4e5f1dc3c1
gfdgd_xi <3025613752@qq.com>
2024-06-01 07:23:40 +0800

    调整翻译

--- i18n/installer-uk.ts
+++ i18n/installer-uk.ts
@@ -250,8 +250,8 @@
     <message>
         <location filename="../src/ui/frames/install_progress_frame.cpp" line="129"/>
         <location filename="../src/ui/frames/install_progress_frame.cpp" line="154"/>
-        <source>You can experience the incredible pleasure of deepin after the time for just a cup of coffee</source>
-        <translation>Ви можете пережити неймовірне задоволення від deepin проводячи час за чашкою кави або чаю</translation>
+        <source>You can experience the incredible pleasure of GXDE after the time for just a cup of coffee</source>
+        <translation>Ви можете пережити неймовірне задоволення від GXDE проводячи час за чашкою кави або чаю</translation>
     </message>
 </context>
 <context>
@@ -265,8 +265,8 @@
     <message>
         <location filename="../src/ui/frames/install_success_frame.cpp" line="43"/>
         <location filename="../src/ui/frames/install_success_frame.cpp" line="63"/>
-        <source>Reboot to enjoy the new experience with deepin, hope you like it!</source>
-        <translation>Перезавантажте, щоб насолодитися новим досвідом з deepin, сподіваюся, вам сподобається!</translation>
+        <source>Reboot to enjoy the new experience with GXDE, hope you like it!</source>
+        <translation>Перезавантажте, щоб насолодитися новим досвідом з GXDE, сподіваюся, вам сподобається!</translation>
     </message>
     <message>
         <location filename="../src/ui/frames/install_success_frame.cpp" line="46"/>
@@ -607,8 +607,8 @@
     </message>
     <message>
         <location filename="../src/ui/frames/disk_space_insufficient_frame.cpp" line="40"/>
-        <source>It needs more than %1GB disk space to install deepin, for better performance, %2GB and more space is recommended</source>
-        <translation>Для встановлення deepin потрібно більше ніж %1ГБ дискового простору для підвищення продуктивності, рекомендується використовувати %2ГБ і більше місця</translation>
+        <source>It needs more than %1GB disk space to install GXDE, for better performance, %2GB and more space is recommended</source>
+        <translation>Для встановлення GXDE потрібно більше ніж %1ГБ дискового простору для підвищення продуктивності, рекомендується використовувати %2ГБ і більше місця</translation>
     </message>
 </context>
 <context>
@@ -676,8 +676,8 @@
     </message>
     <message>
         <location filename="../src/ui/frames/inner/simple_partition_frame.cpp" line="408"/>
-        <source>Windows will not boot if install deepin on Windows disk, please confirm to continue</source>
-        <translation>Windows не завантажиться, якщо встановити deepin на диску Windows, будь ласка, підтвердьте, щоб продовжити</translation>
+        <source>Windows will not boot if install GXDE on Windows disk, please confirm to continue</source>
+        <translation>Windows не завантажиться, якщо встановити GXDE на диску Windows, будь ласка, підтвердьте, щоб продовжити</translation>
     </message>
 </context>
 <context>
@@ -860,8 +860,8 @@
     <message>
         <location filename="../src/ui/frames/virtual_machine_frame.cpp" line="42"/>
         <location filename="../src/ui/frames/virtual_machine_frame.cpp" line="60"/>
-        <source>System has detected that you are using a virtual machine, which will affect the system performance and operation experience, for a smooth experience, it is recommended to install deepin in real-machine environment</source>
-        <translation>Система виявила, що ви використовуєте віртуальну машину, яка вплине на продуктивність системи та досвід роботи, для забезпечення безперебійної роботи, рекомендується встановити deepin в середовищі реального комп&apos;ютера</translation>
+        <source>System has detected that you are using a virtual machine, which will affect the system performance and operation experience, for a smooth experience, it is recommended to install GXDE in real-machine environment</source>
+        <translation>Система виявила, що ви використовуєте віртуальну машину, яка вплине на продуктивність системи та досвід роботи, для забезпечення безперебійної роботи, рекомендується встановити GXDE в середовищі реального комп&apos;ютера</translation>
     </message>
     <message>
         <location filename="../src/ui/frames/virtual_machine_frame.cpp" line="46"/>
@@ -1045,8 +1045,8 @@
     <message>
         <location filename="../src/ui/frames/inner/partition_table_warning_frame.cpp" line="62"/>
         <location filename="../src/ui/frames/inner/partition_table_warning_frame.cpp" line="118"/>
-        <source>Exit BIOS, and enter deepin to install</source>
-        <translation>Вийдіть з BIOS і увійдіть в deepin для установки.</translation>
+        <source>Exit BIOS, and enter GXDE to install</source>
+        <translation>Вийдіть з BIOS і увійдіть в GXDE для установки.</translation>
     </message>
     <message>
         <location filename="../src/ui/frames/inner/partition_table_warning_frame.cpp" line="66"/>
